c6f691ead92f171b0be8c1c83da3a1d8313b46a9
emi-bollas <birdlandbollas@ciencias.unam.mx>
2024-11-25 18:38:37 -0600

    Correción de 4, compleción de 5 y 6. Corrección de indentado.

--- Estructuras Discretas/PFI/PFI.hs
+++ Estructuras Discretas/PFI/PFI.hs
@@ -1,6 +1,5 @@
 --- Emiliano García Bollás
 data Var = A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z deriving (Show, Eq, Ord)
-
 data Formula = Atom Var
                |Neg Formula
                |Formula :&: Formula
@@ -25,52 +24,61 @@ conjunto :: [Var] -> [Var]
 conjunto [] = []
 conjunto (x:xs) = if estaContenido xs x
                   then conjunto xs
-                       else x : conjunto xs
+                  else x : conjunto xs
 -------Auxiliar de conjunto, también de P03.hs-------                            
 estaContenido :: [Var] -> Var -> Bool
 estaContenido [] q = False
 estaContenido (x:xs) q = if x == q
-                            then True
-                                 else estaContenido xs q
+                         then True
+                         else estaContenido xs q
 -----------------------------------------------------
-
 -------------------- EJERCICIO 2 --------------------
 negacion :: Formula -> Formula
 negacion (Atom a) = Neg (Atom a)
-negacion (f1 :&: f2) = negacion f1 :|: negacion f2
-negacion (f1 :|: f2) = negacion f1 :&: negacion f2
-negacion (f1 :=>: f2) = f1 :&: negacion f2
+negacion (Neg fn) = fn
+negacion (f1 :&: f2) = (negacion f1) :|: (negacion f2)
+negacion (f1 :|: f2) = (negacion f1) :&: (negacion f2)
+negacion (f1 :=>: f2) = (f1) :&: (negacion f2)
 negacion (f1 :<=>: f2) = (f1 :&: negacion f2) :|: (negacion f1 :&: f2)
 -----------------------------------------------------
-
 -------------------- EJERCICIO 3 --------------------
 equivalencia :: Formula -> Formula
-equivalencia (Atom a) = Atom a --Posiblemente de más
-equivalencia (f1 :=>: f2) = negacion f1 :|: f2
-equivalencia (f1 :<=>: f2) = (f1 :&: f2) :|: (negacion f1 :&: negacion f2)
+equivalencia (Atom a) = Atom a
+equivalencia (Neg fn) = Neg (equivalencia fn)
+equivalencia (f1 :&: f2) = (equivalencia f1) :&: (equivalencia f2)
+equivalencia (f1 :|: f2) = (equivalencia f1) :|: (equivalencia f2)
+equivalencia (f1 :=>: f2) = negacion (equivalencia f1) :|: (equivalencia f2)
+equivalencia (f1 :<=>: f2) = (equivalencia f1 :&: equivalencia f2) :|: (negacion (equivalencia f1) :&: negacion (equivalencia f2))
 -----------------------------------------------------
-
 -------------------- EJERCICIO 4 --------------------
-interpretacion :: Formula -> [(Var,Bool)] -> Bool
-interpretacion (Atom a :=>: Atom b) [(a,m),(b,n)] = (not m) || n
-interpretacion (Atom a :&: Atom b) [(a,m),(b,n)] = m && n 
-interpretacion (Atom a :|: Atom b) [(a,m),(b,n)] = m || n
-interpretacion (Atom a :<=>: Atom b) [(a,m),(b,n)] = ( (not m) || n) && ( (not n) || m)
-interpretacion (f1 :&: f2) comp = interpretacion f1 comp && interpretacion f2 comp
-interpretacion (f1 :|: f2) comp = interpretacion f1 comp || interpretacion f2 comp
-interpretacion (f1 :=>: f2) comp = not (interpretacion f1 comp) || interpretacion f2 comp
-interpretacion (f1 :<=>: f2) comp = interpretacion (f1 :=>: f2) comp && 
-                                                                  interpretacion (f2 :=>: f1) comp
-                                    
-
+--Función que dado una variable y una lista de tuplas (Var, Bool) devuelve el valor booleano asociado a la variable
+suchen :: Var -> [(Var, Bool)] -> Bool
+suchen a [] = error "No todas las variables están definidas"
+suchen a ((x, boo):xs) = if a == x
+                         then boo 
+                         else suchen a xs
+interpretacion :: Formula -> [(Var, Bool)] -> Bool
+interpretacion (Atom a) con = suchen a con
+interpretacion (Neg fn) con = not (interpretacion fn con)
+interpretacion (f1 :&: f2) vals = (interpretacion f1 vals) && (interpretacion f2 vals)
+interpretacion (f1 :|: f2) vals = (interpretacion f1 vals) || (interpretacion f2 vals)
+interpretacion (f1 :=>: f2) vals = not (interpretacion f1 vals) || (interpretacion f2 vals)
+interpretacion (f1 :<=>: f2) vals = (interpretacion f1 vals) == (interpretacion f2 vals)
 -----------------------------------------------------
-
 -------------------- EJERCICIO 5 --------------------
 combinaciones :: Formula -> [[(Var,Bool)]]
-combinaciones _ = undefined
+combinaciones fn = prol (asociacion (variables fn))
+--Función auxiliar que recibe una lista de variables y devuelve una tupla de la misma con un valor booleano
+asociacion :: [Var] -> [[(Var, Bool)]]
+asociacion [] = [[]]
+asociacion (x:xs) = [(x, False):ys | ys <- asociacion xs] ++ [(x, True):ys | ys <- asociacion xs]
+--Función auxiliar que continúa agrupando tuplas de variables y booleanos
+prol :: [[(Var, Bool)]] -> [[(Var, Bool)]]
+prol [] = []
+prol (x:xs) = x : prol xs
 -----------------------------------------------------
-
 -------------------- EJERCICIO 6 --------------------
 tablaDeVerdad :: Formula -> [([(Var,Bool)],Bool)]
-tablaDeVerdad _ = undefined
+tablaDeVerdad fn = [(x, interpretacion fn x) | x <- combinaciones fn]
 -----------------------------------------------------
+
